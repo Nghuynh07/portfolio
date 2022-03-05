a12aa4da315c2b2473e2839c89c53023bc0fe5eb
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-04 18:01:55 -0800

    another commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,16 @@
 import classes from "./Projects.module.css";
-
+import Layout from "./Layout";
 const Portfolio = () => {
-  return <section className={classes.portfolio}>Projects</section>;
+  return (
+    <Layout>
+      <span></span>
+      <div className={classes.portfolio}>
+        <span className={classes.left}>!</span>! Under Construction !
+        <span className={classes.right}>!</span>
+      </div>
+      <span></span>
+    </Layout>
+  );
 };
 
 export default Portfolio;
