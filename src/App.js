ca09104fe29930853ebf7e402e52223fbd750585
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-29 22:45:42 -0700

    edit layout

--- src/App.js
+++ src/App.js
@@ -7,36 +7,36 @@ function App() {
   return (
     <div className='container'>
       <div className='background'></div>
-      <header className='flex'>
-        <div className='heading-wrapper flex'>
+      <header>
+        <div className='heading-wrapper'>
           <h1 id='frontend'>front-end</h1>
           <h1 id='developer'>developer</h1>
-          <div className='text-wrapper flex'>
+          <div className='text-wrapper'>
             <p className='inter'>
               Interdisciplinary Visual Art and Pharmacy background
             </p>
-            <div className='creativity flex'>
+            <div className='creativity'>
               <p>Creativity</p>
               <p>Simplicity</p>
               <p>Aesthetic</p>
             </div>
           </div>
-        </div>
-        <div className='navigation'>
-          <Link to='/about' className='nav-link flex'>
-            about
-          </Link>
+          <div className='navigation'>
+            <Link to='/about' className='nav-link'>
+              about
+            </Link>
 
-          <Link to='/projects' className='nav-link flex'>
-            projects
-          </Link>
+            <Link to='/projects' className='nav-link'>
+              projects
+            </Link>
 
-          <Link to='/contact' className='nav-link flex'>
-            contact
-          </Link>
+            <Link to='/contact' className='nav-link'>
+              contact
+            </Link>
+          </div>
         </div>
       </header>
-      <div className='components flex'>
+      <div className='components'>
         <Routes>
           <Route path='/about' element={<About />} />
           <Route path='/projects' element={<Projects />} />
--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,19 @@
 const About = () => {
   return (
-    <div className='about flex'>
-      <div className='about-heading flex'>
+    <div className='about'>
+      <div className='about-heading'>
         <h1>Hello</h1>
-        <h1>I'm</h1>
-        <h1 className='section-title'>Huynh</h1>
+        <h1 className='section-title'>I'm Huynh</h1>
         <p>Welcome to my website</p>
       </div>
-      <div className='about-text-container flex'>
+      <div className='about-text-container'>
         <p className='about-text'>
-          Hello There! I am a front-end developer. I remember looking at
-          websites and wondering how these beautiful pages were created. Over
-          the years, my curiosity grew and in March, 2021 I decided to slowly
-          transition my career into programming after 10 years in pharmacy
-          setting. It wasn't easy, but it was a start. I immediately enrolled in
-          coding boot camp and started my programming journey. I fell in love
-          with front-end development during the boot camp.
+          I remember looking at websites and wondering how these beautiful pages
+          were created. Over the years, my curiosity grew and in March, 2021 I
+          decided to slowly transition my career into programming after 10 years
+          in pharmacy setting. It wasn't easy, but it was a start. I immediately
+          enrolled in coding boot camp and started my programming journey. I
+          fell in love with front-end development during the boot camp.
         </p>
       </div>
     </div>
