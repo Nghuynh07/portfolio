148a945d4b1d34d6c9836dac1b6ab8e389307b57
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-04-03 21:38:47 -0700

    more fixes on media queries

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,26 @@
 const About = () => {
   return (
     <div className='about' id='about'>
-      <h1 className='about-title'>About</h1>
-      <div className='about-heading'>
-        <h1>Hello & Welcome</h1>
-        <h1 className='section-title'>I'm Huynh</h1>
-        <p>.H.N.</p>
-      </div>
-      <div className='about-text-container'>
-        <p className='about-text'>
-          I remember looking at websites and wondering how these beautiful pages
-          were created. Over the years, my curiosity grew and in March, 2021 I
-          decided to slowly transition my career into programming after 10 years
-          of pharmacy setting. It wasn't easy, but it was a start. I immediately
-          enrolled in coding boot camp and started my programming journey. I
-          fell in love with front-end development during boot camp and have been
-          honing my skills as front-end developer ever since.
-        </p>
+      <div className='about-wrapper'>
+        <h1 className='about-title'>About</h1>
+        <div className='about-heading'>
+          <h1>Hello & Welcome</h1>
+          <h1 className='section-title'>I'm Huynh</h1>
+          <p>.H.N.</p>
+        </div>
+        <div className='about-text-container'>
+          <p className='about-text'>
+            I remember looking at websites and wondering how these beautiful
+            pages were created. Over the years, my curiosity grew and in March,
+            2021 I decided to slowly transition my career into programming after
+            10 years of pharmacy setting. It wasn't easy, but it was a start. I
+            immediately enrolled in coding boot camp and started my programming
+            journey. I fell in love with front-end development during boot camp
+            and have been honing my skills as front-end developer ever since.
+          </p>
+        </div>
       </div>
     </div>
-    // </div>
   );
 };
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,19 @@ const Contact = () => {
   return (
     <div className='contact' id='contact'>
       <h1 className='contact-title'>Contact</h1>
-      <div className='contact-wrapper'>
-        <h1 className='contact-heading'>Lets Talk</h1>
-        <form className='form'>
-          <div className='input-wrapper'>
-            <input type='text' placeholder='Your Name' />
-          </div>
-          <div className='input-wrapper'>
-            <textarea placeholder='Your message' className='textarea' />
-          </div>
-          <input type='submit' value='Send' className='form-btn' />
-        </form>
+      <div className='contact-container'>
+        <div className='contact-wrapper'>
+          <h1 className='contact-heading'>Lets Talk</h1>
+          <form className='form'>
+            <div className='input-wrapper'>
+              <input type='text' placeholder='Your Name' />
+            </div>
+            <div className='input-wrapper'>
+              <textarea placeholder='Your message' className='textarea' />
+            </div>
+            <input type='submit' value='Send' className='form-btn' />
+          </form>
+        </div>
       </div>
     </div>
   );
