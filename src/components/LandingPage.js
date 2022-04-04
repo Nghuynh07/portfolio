dcb4136ebb65bb8c63bf2a717b15c47acb84a029
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-04-03 20:06:30 -0700

    woohoo!

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,17 +7,35 @@ const LandingPage = () => {
     <>
       <header id='home'>
         <div className='heading-wrapper'>
-          <h1 id='frontend'>front-end</h1>
-          <h1 id='developer'>developer</h1>
+          <div className='frontend'>
+            <h1 className='frontEndDeveloper'>f</h1>
+            <h1 className='frontEndDeveloper'>r</h1>
+            <h1 className='frontEndDeveloper'>o</h1>
+            <h1 className='frontEndDeveloper'>n</h1>
+            <h1 className='frontEndDeveloper'>t</h1>
+            <h1 className='frontEndDeveloper'>-</h1>
+            <h1 className='frontEndDeveloper'>e</h1>
+            <h1 className='frontEndDeveloper'>n</h1>
+            <h1 className='frontEndDeveloper'>d</h1>
+          </div>
+          <div className='developer'>
+            <h1 className='frontEndDeveloper'>d</h1>
+            <h1 className='frontEndDeveloper'>e</h1>
+            <h1 className='frontEndDeveloper'>v</h1>
+            <h1 className='frontEndDeveloper'>e</h1>
+            <h1 className='frontEndDeveloper'>l</h1>
+            <h1 className='frontEndDeveloper'>o</h1>
+            <h1 className='frontEndDeveloper'>p</h1>
+            <h1 className='frontEndDeveloper'>e</h1>
+            <h1 className='frontEndDeveloper'>r</h1>
+          </div>
           <div className='text-wrapper'>
             <p className='inter'>
               Interdisciplinary Visual Art and Pharmacy background
             </p>
-            <div className='creativity'>
-              <p>Creativity</p>
-              <p>Simplicity</p>
-              <p>Aesthetic</p>
-            </div>
+            <p className='inter'>
+              Enjoy minimal designs and feeling good about it
+            </p>
           </div>
           <div className='social-media'>
             <a href='https://github.com/Nghuynh07'>GitHub</a>
