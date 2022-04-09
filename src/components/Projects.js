31c6a66b93d0c0befb3e498714aeb3b9b26eec74
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-04-08 20:40:03 -0700

    added third project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,30 @@
 import evff from "./../img/evff.gif";
 import sweeetmovies from "./../img/sweetmovies.gif";
+import echovalley from "./../img/echovalley.gif";
 
 const Projects = () => {
   return (
     <>
       <div className='projects' id='projects'>
         <h1 className='projects-title'>projects</h1>
-        <div className={`project `}>
+        <div className={`project`}>
           <div className='img-container'>
             <h3>
-              0<span>1</span>
+              0<span>2</span>
             </h3>
-            <img src={evff} alt='projectOne' />
+            <img src={echovalley} alt='projectOne' />
           </div>
           <div className='project-wrapper'>
-            <h1 className='project-title'>Echo Valley Family Farm</h1>
-            <div className='project-1-technologies evff'>
+            <h1 className='project-title'>EVFF</h1>
+            <div className='project-1-technologies sweetmovies'>
               <p>HTML</p>
               <p>CSS</p>
-              <p>Java(Springboot)</p>
+              <p>React</p>
+              <p>MongoDB</p>
+              <p>Express</p>
             </div>
             <a
-              href='https://github.com/Nghuynh07/echovalleyfarm'
+              href='https://github.com/Branley22/mern_group_project'
               className='demo'
             >
               Go To Demo
@@ -47,6 +50,29 @@ const Projects = () => {
             </div>
             <a
               href='https://github.com/Branley22/mern_group_project'
+              className='demo'
+            >
+              Go To Demo
+            </a>
+            <div className='line'></div>
+          </div>
+        </div>
+        <div className={`project `}>
+          <div className='img-container'>
+            <h3>
+              0<span>1</span>
+            </h3>
+            <img src={evff} alt='projectOne' />
+          </div>
+          <div className='project-wrapper'>
+            <h1 className='project-title'>Echo Valley Family Farm</h1>
+            <div className='project-1-technologies evff'>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>Java(Springboot)</p>
+            </div>
+            <a
+              href='https://github.com/Nghuynh07/echovalleyfarm'
               className='demo'
             >
               Go To Demo
