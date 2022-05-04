91a969a90789aac303257b41ac0674253192c76b
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-05-03 22:46:30 -0700

    added observerer api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,37 @@ import Projects from "./components/Projects";
 import Nav from "./components/Nav";
 import LandingPage from "./components/LandingPage";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 import Contact from "./components/Contact";
 import Title from "./components/Title";
 function App() {
+  useEffect(() => {
+    const sections = document.querySelectorAll(".section");
+    sections.forEach((section) => {
+      section.classList.add("hidden");
+    });
+    const sectionCallBack = (entries, observer) => {
+      const [entry] = entries;
+
+      if (!entry.isIntersecting) return;
+
+      entry.target.classList.remove("hidden");
+
+      // observer.unobserve(entry.target);
+    };
+
+    const options = {
+      root: null,
+      threshold: 0.1,
+    };
+
+    const sectionObserver = new IntersectionObserver(sectionCallBack, options);
+
+    sections.forEach((section) => {
+      sectionObserver.observe(section);
+    });
+  }, []);
+
   return (
     <div className='container'>
       <Nav />
@@ -14,7 +42,6 @@ function App() {
       <About />
       <Title title='Projects' />
       <Projects />
-      {/* <Title title="Lets's Talk" /> */}
       <Contact />
       <Footer />
     </div>
